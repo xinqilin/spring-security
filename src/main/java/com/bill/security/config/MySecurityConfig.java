f3360b1cf6d4b2196f968b0d399d4a5343bfce19
BillLin1209 <zzx123bill@gmail.com>
2020-06-20 00:24:04 +0800

    add config

---
+++ src/main/java/com/bill/security/config/MySecurityConfig.java
@@ -0,0 +1,12 @@
+package com.bill.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
+	
+}
